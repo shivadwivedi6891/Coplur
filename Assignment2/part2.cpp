897b0a743c026404e1e0c4a1ab088e0dd5e7d1a8
shivadwivedi6891 <122284144+shivadwivedi6891@users.noreply.github.com>
2025-03-08 12:30:22 +0530

    part2

---
+++ Assignment2/part2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// Step 1: Define an Interface
+class ILogger {
+public:
+    virtual void Log() = 0; // Pure virtual function
+};
+
+
+class DatabaseLogger : public ILogger {
+public:
+    void Log() override {
+        cout << "Log Written to Database " << endl;
+    }
+};
+
+class FileLogger : public ILogger {
+public:
+    void Log() override {
+        cout << "Log Written to File " << endl;
+    }
+};
+
+class CloudLogger : public ILogger {
+public:
+    void Log() override {
+        cout << "Log Written to Cloud " << endl;
+    }
+};
+
+
+int main() {
+    string userLog;
+    cout << "Enter the Log type : ";
+    cin >> userLog;
+
+    ILogger* log = nullptr; // 
+
+    // Step 4: Assign correct logger
+    if (userLog == "Database") {
+        log = new DatabaseLogger();
+    } else if (userLog == "File") {
+        log = new FileLogger();
+    } else if (userLog == "Cloud") {
+        log = new CloudLogger();
+    }
+
+   
+    if (log != nullptr) {
+        log->Log();
+        delete log; 
+    } else {
+        cout << "Invalid Log Type!" << endl;
+    }
+
+
+}
