59a12a601588bf3586157051bae8ba11d5478ab8
shivadwivedi6891 <122284144+shivadwivedi6891@users.noreply.github.com>
2025-05-01 08:14:06 +0530

    update

--- ReactAssignment-3/react/src/Components/Calculator.jsx
+++ ReactAssignment-3/react/src/Components/Calculator.jsx
@@ -22,27 +22,27 @@ const Calculator = () => {
             <input type="text" style ={{width:"270px"}} className='input'   value = {value} />
             </div>
 
-            <div >
+            <div className='btn-cal'>
                 <input type="button" style ={{background:"red",width:"60px"}} value="AC" onClick={e=>setValue('')} />
-                <input type="button" style = {{width:"60px"}} value="DE" onClick={e=>setValue(value.slice(0,-1))} />
-                <input type="button" style = {{width:"60px"}} value="." onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="/" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="DE" onClick={e=>setValue(value.slice(0,-1))} />
+                <input type="button"  value="." onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="/" onClick={e=>setValue(value + e.target.value)} />
 
             </div>
 
-            <div>
-                <input type="button"  style = {{width:"60px"}}  value="7" onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="8" onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="9" onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="*" onClick={e=>setValue(value + e.target.value)} />
+            <div className='btn-cal'>
+                <input type="button"   value="7" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="8" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="9" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="*" onClick={e=>setValue(value + e.target.value)} />
 
             </div>
 
-            <div>
-                <input type="button" style = {{width:"60px"}} value="4" onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="5" onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="6" onClick={e=>setValue(value + e.target.value)} />
-                <input type="button" style = {{width:"60px"}} value="+" onClick={e=>setValue(value + e.target.value)} />
+            <div className='btn-cal'>
+                <input type="button" value="5" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="6" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="+" onClick={e=>setValue(value + e.target.value)} />
+                <input type="button"  value="4" onClick={e=>setValue(value + e.target.value)} />
 
             </div>
 
